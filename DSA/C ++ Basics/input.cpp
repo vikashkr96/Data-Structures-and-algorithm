a17f370d01cb82fd4fad8fed2a1fc5e2c6f52f7b
Vikash Kumar <178039979+vikashkr96@users.noreply.github.com>
2025-07-19 11:36:46 +0530

    Add files via upload

---
+++ DSA/C ++ Basics/input.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    // char name;
+    // int age;
+    // cout<< "Enter the name : "<<endl;
+    // cin>> name ;
+    // cout<< "Enter the age : "<<endl;
+    // cin>> age ;
+    // cout<< "Your name is : "<<name << endl;
+    // cout<< "Your age is : "<<age << endl;
+
+    // pre increament
+
+    
+
+    // post increament 
+
+    int c = 10;
+    int d = c++;
+    cout<< "d is "<<d<<endl ;
+    cout<< "c is "<<c;
+    return 0;
+}
---
+++ DSA/C ++ Basics/lec_1.cpp
@@ -0,0 +1,16 @@
+# include <iostream>
+using namespace std;
+
+int main(){
+    // variables ....
+    //  and datatypes ....
+    int a = 10;
+    float PI = 3.14f;
+
+    // type casting  .....
+    char q = 'A';
+    int value = q;
+    
+    cout<< value ;
+    return 0;
+}
