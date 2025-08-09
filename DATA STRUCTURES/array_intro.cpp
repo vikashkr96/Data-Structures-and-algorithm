5cd4bf54dd8af2c2a11a0c5bf19c993202c6e80a
Vikash Kumar <178039979+vikashkr96@users.noreply.github.com>
2025-08-09 23:27:02 +0530

    Add files via upload

---
+++ DATA STRUCTURES/array_intro.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int marks[5] = {87, 89, 39, 69, 97};
+    // cout<< marks[4] << endl;
+    // cout<< marks[3]<<endl;
+
+    for(int i =0; i<5 ; i++)
+    cout<<marks[i]<<endl;
+    return 0;
+}
