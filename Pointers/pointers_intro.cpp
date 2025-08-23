73e3774f3dd1c4a6ed97982f02574f18a802fd22
Vikash Kumar <178039979+vikashkr96@users.noreply.github.com>
2025-08-23 12:04:45 +0530

    Add files via upload

---
+++ Pointers/pointers_intro.cpp
@@ -0,0 +1,67 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main(){
+    // int a = 10;
+    // cout<< &a <<endl;
+    // ----------------------------------------
+
+    // int a = 10;
+    // int* ptr = &a;
+    // cout<< ptr <<endl;
+    // cout<< &a <<endl;
+    // cout<< &ptr <<endl;
+    //------------------------------------------
+
+    // int a = 10;
+    // int* ptr = &a;
+    // int** ptr1 = &ptr;
+
+    // cout<< &a <<endl;
+    // cout<< ptr <<endl;
+
+    // cout<< &ptr <<endl;
+    // cout<< ptr1 <<endl;
+
+    // cout<< &ptr1 <<endl;
+    // int*** ptr2 = &ptr1;
+    // cout<< ptr2 <<endl;
+    // // -------------------------------------------------
+
+    // use dereferance operator 
+    // int a = 10;
+    // int* ptr = &a;
+    // cout<< *(&a) <<endl;
+    // cout<< *(ptr)<<endl;
+
+    // int** ptr1 = &ptr;
+    // cout<< *(ptr1) <<endl;
+    // cout<< ptr <<endl;
+
+    // cout<< **(ptr1) <<endl;
+    // cout<< *(ptr) <<endl;
+
+
+    //----------------------------------
+
+    int** ptr;
+    cout<<ptr<<endl; // garbage value
+
+    int** ptr1  = NULL;
+    cout<< ptr1 <<endl;
+
+
+
+
+
+
+
+
+
+
+
+
+
+    return 0;
+}
